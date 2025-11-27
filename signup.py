43fe9e5df7b6039d56375f064445248cae03706d
6320331 <rodaaron936@gmail.com>
2025-11-27 14:53:27 -0500

    v0.6A

--- signup.py
+++ signup.py
@@ -12,12 +12,12 @@ from tkinter import ttk, messagebox
 cette classe sort un écran d'inscription. Il faut un prénom, nom, email et mot de passe à double
 validation (min. 5 chars). Il doit aussi cocher la boite pour accepter les conditions d'utilisateur
 """
-class SignupWindow(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.window = tk.Toplevel()
-        self.window.title("Inscription")
-        self.window.geometry("300x200")
+class SignupWindow(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.title("Inscription")
+        self.geometry("300x200")
         self.build_ui()
 
     def build_ui(self):
@@ -56,9 +56,20 @@ class SignupWindow(tk.Tk):
         self.mdp_check_en = ttk.Entry(self.frm, width=15)
         self.mdp_check_en.grid(row=4, column=1, sticky="nsew")
 
-        #chechbutton
-        self.chk_lb = ttk.Checkbutton(self.frm, text="Valider", variable=self)
-        self.chk_lb.grid(row=5, column=1, sticky="nsew")
+        #chechbutton et confirm
+        self.is_valid = tk.BooleanVar(value=False)
+        self.chk = ttk.Checkbutton(self.frm, text="Valider", variable=self.is_valid)
+        self.chk.grid(row=5, column=0, sticky="nsew")
+        self.chk.bind("<Button-1>", lambda e: self.update_btn())
+
+        self.signup_bt = ttk.Button(self.frm, text="s'inscrire", width=15, state='disabled')
+        self.signup_bt.grid(row=5, column=1, sticky="nsew")
+
+    def update_btn(self):
+        if not self.is_valid.get():
+            self.signup_bt.configure(state='active')
+        else:
+            self.signup_bt.configure(state='disabled')
 
 
 if __name__ == '__main__':
